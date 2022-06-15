b5a12cf8d2896d8657eedbf97ea4ed38377c3cfe
mollyxu <molly.xu@uwaterloo.ca>
2022-06-15 00:24:33 -0700

    Completed step 4 - query the Parse database

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -10,6 +10,7 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("GP6n0uhX7qIw2vGVbqAWA26FepxjGxBiZAvMKP8F")
                 .clientKey("51egfDMLDuFw3CPrCT2g2z4BiebvO2hL8IjdWMUq")
