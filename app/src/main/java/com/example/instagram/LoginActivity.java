449ecb5a9fdf3c73ca484e2cb2b6c93e004e5c7f
mollyxu <molly.xu@uwaterloo.ca>
2022-06-16 15:31:58 -0700

    completed UI revamp haha

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -26,10 +26,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.nav_logo_whiteout);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setTitle("");
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setLogo(R.drawable.nav_logo_whiteout);
+//        getSupportActionBar().setDisplayUseLogoEnabled(true);
+//        getSupportActionBar().setTitle("");
+        // hide actionbar here
+        getSupportActionBar().hide();
 
         if (ParseUser.getCurrentUser() != null) {
             goFeedActivity();
