3a329703ec0a4c954da6c575bfda46496f3b711c
mollyxu <molly.xu@uwaterloo.ca>
2022-06-15 21:47:51 -0700

    styling WIP, considering a switch to toolbar

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -26,6 +26,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.nav_logo_whiteout);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setTitle("");
+
         if (ParseUser.getCurrentUser() != null) {
             goFeedActivity();
         }
